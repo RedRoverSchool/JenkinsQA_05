b6397000518556333df2f2e8c553b37b80414aea
Radas-I <ionrd78@gmail.com>
2022-11-02 23:22:06 +0000

    add new test

--- src/test/java/BugBustersTest.java
+++ src/test/java/BugBustersTest.java
@@ -6,7 +6,7 @@ import runner.BaseTest;
 public class BugBustersTest extends BaseTest {
 
     @Test
-    public void testArailymSuccessLogIn(){
+    public void testArailymSuccessLogIn() {
         getDriver().get("https://katalon-demo-cura.herokuapp.com/");
         getDriver().findElement(By.xpath("//body/a/i")).click();
         getDriver().findElement(By.xpath("//body/nav/ul/li/a[@href = 'profile.php#login']")).click();
@@ -14,6 +14,22 @@ public class BugBustersTest extends BaseTest {
         getDriver().findElement(By.id("txt-password")).sendKeys("ThisIsNotAPassword");
         getDriver().findElement(By.id("btn-login")).click();
         Assert.assertEquals(getDriver().findElement(By.xpath("//h2")).getText(), "Make Appointment");
+
+    }
+
+    @Test
+    public void testRadasSuccessLogIn() throws InterruptedException {
+        getDriver().get("https://www.wunderground.com/");
+        getDriver().findElement(By.xpath("//div[@id='global-header']/lib-login/div/p/span/a")).click();
+        getDriver().findElement(By.xpath("//button[@id='truste-consent-button']")).click();
+        Thread.sleep(1000);
+        getDriver().findElement(By.xpath("//input[@id='form-signin-email']")).sendKeys("motoxx68@gmail.com");
+        getDriver().findElement(By.xpath("//input[@id='form-signin-password']")).sendKeys("Intsnewpassword");
+        Thread.sleep(1000);
+        getDriver().findElement(By.xpath("//input[@id='signIn'][@class='button']")).click();
+        Thread.sleep(1000);
+        getDriver().findElement(By.xpath("//div[@id = 'global-header']//button[@class = 'wu-account close-login ng-star-inserted']")).click();
+        Assert.assertEquals(getDriver().findElement(By.xpath("//li[@translatecontext='wu-header-user-login']")).getText(), "Welcome back!");
 
     }
 }
