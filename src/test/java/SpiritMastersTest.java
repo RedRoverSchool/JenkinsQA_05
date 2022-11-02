4e2ac3ad15b1b3dbd7f7b9641c9c4e556fa0d447
MaximWho <atom_0071@yahoo.com>
2022-11-02 19:05:48 +0100

    addedBankTest

--- src/test/java/SpiritMastersTest.java
+++ src/test/java/SpiritMastersTest.java
@@ -1,6 +1,9 @@
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
@@ -63,7 +66,7 @@ public class SpiritMastersTest extends BaseTest {
         String actualTextInteractions = getDriver().findElement(By.className("main-header")).getText();
         Assert.assertEquals(actualTextInteractions, "Interactions");
     }
-
+    @Ignore
     @Test
     public void test_PK_RedirectToBooksTab(){
         findCard_PK(5).click();
@@ -86,4 +89,73 @@ public class SpiritMastersTest extends BaseTest {
                 "[href^=\"/butt\"]"));
         Assert.assertEquals(link.getText(), "Buttons");
     }
+
+    @Test
+    public void zyzBank_MW_Test()  {
+
+        getDriver().get("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login");
+
+        WebElement createUser = getDriver().findElement(By.xpath("//button[normalize-space()='Bank Manager Login']"));
+
+        createUser.click();
+
+        WebElement addCastomer = getDriver().findElement(By.xpath("//button[normalize-space()='Add Customer']"));
+
+        addCastomer.click();
+
+        WebElement firstName = getDriver().findElement(By.xpath("//input[@placeholder='First Name']"));
+
+        firstName.click();
+
+        firstName.sendKeys("John");
+
+        WebElement lastName = getDriver().findElement(By.xpath("//input[@placeholder='Last Name']"));
+
+        lastName.click();
+
+        lastName.sendKeys("NeJonh");
+
+        WebElement postcode = getDriver().findElement(By.xpath("//input[@placeholder='Post Code']"));
+
+        postcode.click();
+
+        postcode.sendKeys("12334");
+
+        WebElement conf = getDriver().findElement(By.xpath("//button[@type='submit']"));
+
+        conf.click();
+
+        Alert confallert = getDriver().switchTo().alert();
+
+        confallert.accept();
+
+        WebElement verification = getDriver().findElement(By.xpath("//button[@class='btn btn-lg tab btn-primary']"));
+
+        verification.click();
+
+        WebElement home = getDriver().findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/button[1]"));
+
+        home.click();
+
+        WebElement login = getDriver().findElement(By.xpath("//button[normalize-space()='Customer Login']"));
+
+        Assert.assertEquals(login.getText(), "Customer Login");
+
+        login.click();
+
+        WebElement selectNameVariant = getDriver().findElement(By.id("userSelect"));
+
+        Select dropdown = new Select(selectNameVariant);
+
+        dropdown.selectByValue("6");
+
+        WebElement loginn = getDriver().findElement(By.xpath("//button[@type='submit']"));
+
+        loginn.click();
+
+        Assert.assertEquals(getDriver().findElement(By.xpath("/html/body/div/div/div[2]/div/div[1]/strong/span")).getText(), "John NeJonh");
+
+    }
+
+
 }
