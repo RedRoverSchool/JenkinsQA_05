502a4edba1c102d8ed1a8836334e3431e6787a02
Radas-I <ionrd78@gmail.com>
2022-11-03 17:41:10 +0000

    add new test

--- src/test/java/GroupBugBustersTest.java
+++ src/test/java/GroupBugBustersTest.java
@@ -60,7 +60,7 @@ public class GroupBugBustersTest extends BaseTest {
     public void testRadasSuccessLogIn() throws InterruptedException {
         getDriver().get("https://www.wunderground.com/");
         getDriver().findElement(By.xpath("//div[@id='global-header']/lib-login/div/p/span/a")).click();
-        getDriver().findElement(By.xpath("/html/body/div[1]/div/div/div/div/div/div[3]/button[1]")).click();
+        getDriver().findElement(By.xpath("//button[@id='truste-consent-button']")).click();
         Thread.sleep(2000);
         getDriver().findElement(By.xpath("//input[@id='form-signin-email']")).sendKeys("motoxx68@gmail.com");
         getDriver().findElement(By.xpath("//input[@id='form-signin-password']")).sendKeys("Intsnewpassword");
