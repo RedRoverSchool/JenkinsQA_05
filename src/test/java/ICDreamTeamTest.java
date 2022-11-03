4009d5b7c8ba4bd05542f7858e7ea039fba87555
InaChemerko <innachemerko@gmail.com>
2022-11-02 22:04:09 -0700

    added dreamTeam test

--- src/test/java/ICDreamTeamTest.java
+++ src/test/java/ICDreamTeamTest.java
@@ -1,9 +1,8 @@
-package runner;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import runner.BaseTest;
 
 public class ICDreamTeamTest extends BaseTest {
 
