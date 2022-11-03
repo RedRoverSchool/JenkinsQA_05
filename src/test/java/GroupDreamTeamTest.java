060f78184cb4e7cc4252c99a4b6953a0a1657984
Elena Grueber <elenagrueber@gmail.com>
2022-11-02 23:16:41 -0400

    add test

---
+++ src/test/java/GroupDreamTeamTest.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+
+    public class GroupDreamTeamTest extends BaseTest {
+
+        @Test
+        public void testSelenium() {
+
+            getDriver().get("https://www.selenium.dev/selenium/web/web-form.html");
+
+            WebElement textInput = getDriver().findElement(By.id("my-text-id"));
+            textInput.clear();
+            textInput.sendKeys("Hello everyone");
+
+            WebElement dropdownSelect = getDriver().findElement(By.name("my-select"));
+            Select sel = new Select(dropdownSelect);
+            sel.selectByValue("1");
+            WebElement one = getDriver().findElement(By.xpath("//option[@value='1']"));
+
+            Assert.assertTrue(one.isSelected());
+        }
+}
