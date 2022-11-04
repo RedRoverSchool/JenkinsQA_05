4a545a3ad36cb1c0e5f3290c43f5743e85c0c374
NazLukpan <naz.lukpanova@gmail.com>
2022-11-03 20:40:09 -0400

    first test

---
+++ src/test/java/GroupBughuntersTest.java
@@ -0,0 +1,52 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+public class GroupBughuntersTest extends BaseTest {
+
+    @Test
+    public void ticketonTest() throws InterruptedException {
+        getDriver().get("https://ticketon.kz/");
+        WebElement search = getDriver().findElement(By.name("q"));
+        WebElement button = getDriver().findElement(By.xpath("//button[@class='button postfix secondary search__postfix']"));
+        search.sendKeys("Maneskin");
+        button.click();
+        Assert.assertEquals("Поиск - Система онлайн-покупки билетов в кино и на концерты Ticketon.kz", getDriver().getTitle());
+
+    }
+
+    @Test
+    public void firstTest() throws InterruptedException {
+        getDriver().get("https://www.bbc.co.uk/learningenglish/english/");
+        String text = getDriver().findElement(By.id("heading-things-you-cant-miss")).getText();
+        String actualResult = "THINGS YOU CAN'T MISS";
+        Assert.assertEquals(actualResult, text);
+
+    }
+
+    @Test
+    public void bbcSearch() throws InterruptedException {
+        getDriver().get("https://www.bbc.co.uk/learningenglish/english/");
+        WebElement search = getDriver().findElement(By.xpath("//div/div/form/input[@name='q']"));
+        WebElement button = getDriver().findElement(By.xpath("//div/div/form/input[@name ='submit']"));
+        search.sendKeys("newspaper");
+        button.click();
+        Assert.assertEquals("BBC Learning English - Search", getDriver().getTitle());
+
+    }
+
+    public void greatSchoolMainPageTest() {
+        getDriver().get("https://www.greatschools.org");
+        WebElement searchBox = getDriver().findElement(By.xpath("//*[@class=\"full-width pam search_form_field\"]"));
+        WebElement searchButton = getDriver().findElement(By.xpath("//*[@class=\"search-label\"]"));
+        searchBox.sendKeys("06032");
+        searchButton.click();
+        Assert.assertEquals("Schools in 06032, 1-20 | GreatSchools", getDriver().getTitle());
+
+
+    }
+}
