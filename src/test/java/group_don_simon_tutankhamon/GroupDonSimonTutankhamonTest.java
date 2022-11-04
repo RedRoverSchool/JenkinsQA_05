74a7668b481c198d81484d3b823b8ca00f50dfd5
iKrlkv <il9.korolkov@gmail.com>
2022-11-05 00:32:41 +0500

    remove unstable tests, add testFirstNameInputField_SelectorsHubCom, testIfPageSourceContainsHtmlTag_SelectorsHubCom, testSessionId_SelectorsHubCom, testFooter_SelectorsHubCom

--- src/test/java/group_don_simon_tutankhamon/GroupDonSimonTutankhamonTest.java
+++ src/test/java/group_don_simon_tutankhamon/GroupDonSimonTutankhamonTest.java
@@ -1,5 +1,3 @@
-package group_don_simon_tutankhamon;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
@@ -7,7 +5,6 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.remote.SessionId;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 import java.util.List;
