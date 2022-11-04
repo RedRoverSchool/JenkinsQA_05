dc97212369d31c47bb5cca79cd63a2c71c20bef3
dmswlpk <103320248+dmswlpk@users.noreply.github.com>
2022-11-03 19:02:25 -0600

    first test

--- src/test/java/PLGroupTest.java
+++ src/test/java/PLGroupTest.java
@@ -1,5 +1,35 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 import runner.BaseTest;
+
+import java.util.List;
 
 public class PLGroupTest extends BaseTest {
 
+    @Test
+    public void testH2HeaderSeleniumInteractionsWindows(){
+        getDriver().get("https://www.selenium.dev/documentation/webdriver/interactions/windows/");
+
+        String expectedResult = "Working with windows and tabs";
+
+        Assert.assertEquals(getDriver().findElement(By.xpath("//main/div/h1")).getText(),expectedResult);
+    }
+
+    @Test void testNavTest_Main(){
+        getDriver().get("https://www.selenium.dev/documentation/webdriver/interactions/windows/");
+
+        String expectedResult = "Java".concat("Python");
+
+        List<WebElement> countTableColumns = getDriver().findElements(By.xpath("//main//ul[@id='tabs-0']/li"));
+
+        String columnNames = "";
+        for (WebElement name : countTableColumns) {
+            columnNames += name.getText();
+        }
+
+        Assert.assertTrue(columnNames.contains(expectedResult));
+        Assert.assertEquals(countTableColumns.size(), 6);
+    }
 }
