cacc455405766956df20846e6a07332bd0d1e5c0
NazLukpan <naz.lukpanova@gmail.com>
2022-11-03 21:36:56 -0400

    first test

--- src/test/java/GroupBughuntersTest.java
+++ src/test/java/GroupBughuntersTest.java
@@ -7,7 +7,7 @@ import runner.BaseTest;
 public class GroupBughuntersTest extends BaseTest {
 
     @Test
-    public void ticketonSearch() throws InterruptedException {
+    public void ticketonSearch(){
         getDriver().get("https://ticketon.kz/");
         WebElement search = getDriver().findElement(By.name("q"));
         WebElement button = getDriver().findElement(By.xpath("//button[@class='button postfix secondary search__postfix']"));
@@ -18,7 +18,7 @@ public class GroupBughuntersTest extends BaseTest {
     }
 
     @Test
-    public void bbcHeading()   {
+    public void bbcHeading(){
         getDriver().get("https://www.bbc.co.uk/learningenglish/english/");
         String text = getDriver().findElement(By.id("heading-things-you-cant-miss")).getText();
         String actualResult = "THINGS YOU CAN'T MISS";
@@ -27,7 +27,7 @@ public class GroupBughuntersTest extends BaseTest {
     }
 
     @Test
-    public void bbcSearch() {
+    public void bbcSearch(){
         getDriver().get("https://www.bbc.co.uk/learningenglish/english/");
         WebElement search = getDriver().findElement(By.xpath("//div/div/form/input[@name='q']"));
         WebElement button = getDriver().findElement(By.xpath("//div/div/form/input[@name ='submit']"));
@@ -38,7 +38,7 @@ public class GroupBughuntersTest extends BaseTest {
     }
 
     @Test
-    public void greatSchoolMainPage() {
+    public void greatSchoolMainPage(){
         getDriver().get("https://www.greatschools.org");
         WebElement searchBox = getDriver().findElement(By.xpath("//*[@class=\"full-width pam search_form_field\"]"));
         WebElement searchButton = getDriver().findElement(By.xpath("//*[@class=\"search-label\"]"));
