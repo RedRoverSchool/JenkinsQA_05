1a03983a524940988d5d4f0cbd471c08da07c25b
Arailym Ashimova <arajlymasimova@gmail.com>
2022-11-04 23:49:25 +0600

    check address

--- src/test/java/GroupBugBustersTest.java
+++ src/test/java/GroupBugBustersTest.java
@@ -69,4 +69,15 @@ public class GroupBugBustersTest extends BaseTest {
         Assert.assertEquals(getDriver().findElement(By.xpath("//li[@translatecontext='wu-header-user-login']")).getText(), "Welcome back!");
 
     }
+    @Test
+    public void testArailymCheckAddress(){
+        getDriver().get("https://katalon-demo-cura.herokuapp.com/");
+        String addressLogOut = getDriver().findElement(By.xpath("//body/footer/div/div/div/p[1]")).getText();
+        getDriver().findElement(By.id("btn-make-appointment")).click();
+        getDriver().findElement(By.id("txt-username")).sendKeys("John Doe");
+        getDriver().findElement(By.id("txt-password")).sendKeys("ThisIsNotAPassword");
+        getDriver().findElement(By.id("btn-login")).click();
+        String addressLogIn = getDriver().findElement(By.xpath("//body/footer/div/div/div/p[1]")).getText();
+        Assert.assertEquals(addressLogOut, addressLogIn);
+    }
 }
