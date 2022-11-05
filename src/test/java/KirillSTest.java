a941aa985c8b5306a3e0d6ae41f042864ef927ab
kirill <kirillsumersky@gmail.com>
2022-11-04 22:56:11 -0400

    add kstest

---
+++ src/test/java/KirillSTest.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+public class KirillSTest extends BaseTest {
+
+
+    @Test
+    public void testSomething() {
+
+        getDriver().get("https://weather.com/");
+
+        WebElement link = getDriver().findElement(By.xpath("//a[@href='https://newsroom.ibm.com/the-weather-company']"));
+
+        Assert.assertEquals(link.getText(), "Press Room");
+    }
+
+}
