7d223dc7eae68b0fc216cf4e3a9bd82cd25d6b08
Turii <o.turij@gmail.com>
2022-11-05 20:54:00 -0700

    Add TuriiTest

---
+++ src/test/java/TuriiTest.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+public class TuriiTest extends BaseTest {
+    @Test
+    public void testHerokuApps() {
+        getDriver().get("https://formy-project.herokuapp.com/");
+
+        WebElement link = getDriver().findElement(By.xpath("//li/a[@href='/autocomplete']"));
+
+        Assert.assertEquals(link.getText(), "Autocomplete");
+    }
+}
