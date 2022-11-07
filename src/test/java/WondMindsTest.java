46cc03f9aeb2f7de438fd26b16d711c5875a61c6
Yuliya <julia.shershen@gmail.com>
2022-11-07 13:20:59 -0800

    testGuide() added

--- src/test/java/WondMindsTest.java
+++ src/test/java/WondMindsTest.java
@@ -1,7 +1,5 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
@@ -15,5 +13,27 @@ public class WondMindsTest extends BaseTest {
         search.sendKeys("Тула\n");
         String actualText = getDriver().findElement(By.xpath("//h1")).getText();
         Assert.assertEquals(actualText, "Search result");
+    }
+
+    @Test
+    public void testGuide() throws InterruptedException {
+        String url = "https://openweathermap.org/";
+        String expectedResultTitle = "OpenWeatherMap API guide - OpenWeatherMap";
+        String expectedResultUrl = "https://openweathermap.org/guide";
+
+        getDriver().get(url);
+        getDriver().manage().window().maximize();
+        Thread.sleep(2000);
+
+        WebElement searchButton = getDriver().findElement(
+                By.xpath("//a[@href='/guide']"));
+        searchButton.click();
+        Thread.sleep(1000);
+
+        String actualResultTitle = getDriver().getTitle();
+        Assert.assertEquals(actualResultTitle, expectedResultTitle);
+
+        String actualResultUrl = getDriver().getCurrentUrl();
+        Assert.assertEquals(actualResultUrl, expectedResultUrl);
     }
 }
