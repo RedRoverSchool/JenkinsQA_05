75f6ce8b2e9926a21edc33836fb3fed8e4264b16
janarkqa <janarkqa@gmail.com>
2022-11-08 08:33:07 -0600

    Rename function

--- src/test/java/group_a_team_test/SauceDemoConsts.java
+++ src/test/java/group_a_team_test/SauceDemoConsts.java
@@ -5,6 +5,6 @@ public final class SauceDemoConsts {
     public static final String INVENTORY_PAGE_URL = "https://www.saucedemo.com/inventory.html";
     public static final String STANDARD_USER = "standard_user";
     public static final String CORRECT_PASSWORD = "secret_sauce";
-    public static final String WRONG_USERNAME_OR_PASSWORD_EXCEPTION_MSG =
+    public static final String WRONG_USERNAME_OR_PASSWORD_ERROR_MSG =
             "Epic sadface: Username and password do not match any user in this service";
 }
--- src/test/java/group_a_team_test/SauceDemoLoginPageTest.java
+++ src/test/java/group_a_team_test/SauceDemoLoginPageTest.java
@@ -15,27 +15,27 @@ public class SauceDemoLoginPageTest extends SauceDemoBaseTest {
     @Test
     public void testLockedOutUserLoginIn() {
         loginIn("locked_out_user", SauceDemoConsts.CORRECT_PASSWORD);
-        assertRaiseException("Epic sadface: Sorry, this user has been locked out.");
+        assertShowErrorMsg("Epic sadface: Sorry, this user has been locked out.");
     }
 
     @Test
     public void testNonExistentUserLoginIn() {
         loginIn("not_existent_user", SauceDemoConsts.CORRECT_PASSWORD);
-        assertRaiseException(SauceDemoConsts.WRONG_USERNAME_OR_PASSWORD_EXCEPTION_MSG);
+        assertShowErrorMsg(SauceDemoConsts.WRONG_USERNAME_OR_PASSWORD_ERROR_MSG);
     }
 
     @Test
     public void testLoginInWithWrongPassword() {
         loginIn(SauceDemoConsts.STANDARD_USER, "secret_Sauce");
-        assertRaiseException(SauceDemoConsts.WRONG_USERNAME_OR_PASSWORD_EXCEPTION_MSG);
+        assertShowErrorMsg(SauceDemoConsts.WRONG_USERNAME_OR_PASSWORD_ERROR_MSG);
     }
 
-    private void assertRaiseException(String exception) {
+    private void assertShowErrorMsg(String errorMsg) {
         try {
             Assert.assertTrue(getDriver().findElement(By.xpath("//div[@class='error-message-container error']")).isDisplayed());
-            Assert.assertEquals(getDriver().findElement(By.cssSelector("h3[data-test=error]")).getText(), exception);
+            Assert.assertEquals(getDriver().findElement(By.cssSelector("h3[data-test=error]")).getText(), errorMsg);
         } catch (NoSuchElementException noSuchElementException) {
-            Assert.fail("Exception wasn't raised");
+            Assert.fail("Error message wasn't raised");
         }
     }
 }
