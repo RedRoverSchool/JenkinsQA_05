43d7282570b660ef07b16a18a1aa16a0b38a24b1
Valentin Kuprin <cricket666@yandex.ru>
2022-11-08 21:49:01 +0300

    popUp getText

--- src/test/java/GroupJavaStartTest.java
+++ src/test/java/GroupJavaStartTest.java
@@ -4,12 +4,16 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
+import java.util.Set;
+
 public class GroupJavaStartTest extends BaseTest {
 
-    @Test
-    public void testHerokuApp() {
+    private final String FARM_PROJECT_URL = "https://formy-project.herokuapp.com/";
 
-        getDriver().get("https://formy-project.herokuapp.com/");
+    @Test
+    public void test_HerokuApp() {
+
+        getDriver().get(FARM_PROJECT_URL);
 
         WebElement link = getDriver().findElement(By.xpath("//li/a[@href='/autocomplete']"));
 
@@ -17,22 +21,48 @@ public class GroupJavaStartTest extends BaseTest {
     }
 
     @Test
-    public void testHerokuAppSearchCheckboxMenu() {
-        getDriver().get("https://formy-project.herokuapp.com/");
+    public void test_HerokuAppSearchCheckboxMenu() {
+        getDriver().get(FARM_PROJECT_URL);
 
-        //Thread.sleep(5000);
         WebElement link1 = getDriver().findElement(By.xpath("//li/a[@href='/checkbox']"));
 
         Assert.assertEquals(link1.getText(), "Checkbox");
     }
 
-
     @Test
-    public void testHerokuAppComponentsList(){
-        getDriver().get("https://formy-project.herokuapp.com/");
+    public void test_HerokuAppComponentsList(){
+        getDriver().get(FARM_PROJECT_URL);
 
         WebElement buttons = getDriver().findElement(By.xpath("//li/a[text()='Buttons']"));
 
         Assert.assertEquals(buttons.getText(), "Buttons");
+    }
+
+    @Test
+    public void test_GoToPopUpWindow() throws InterruptedException {
+        getDriver().get("http://omayo.blogspot.com/");
+
+        String mainWindow = getDriver().getWindowHandle();
+
+        getDriver().findElement(
+                By.xpath("//a[text()= 'Open a popup window']")).click();
+
+        Set<String> windows = getDriver().getWindowHandles();
+        String popUpWindow = "";
+
+        for (String changeWindow : windows) {
+            if (!mainWindow.equals(changeWindow)) {
+                popUpWindow = changeWindow;
+                break;
+            }
+        }
+
+        getDriver().switchTo().window(popUpWindow);
+
+        String text = getDriver().findElement(By.id("para1")).getText();
+        String text2 = getDriver().findElement(By.id("para2")).getText();
+
+        Assert.assertEquals(text, "A paragraph of text");
+        Assert.assertEquals(text2, "Another paragraph of text");
     }
 }
