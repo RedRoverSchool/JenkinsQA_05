453d4f71f54406ef73f1a0dc9e7d269311425a07
AlekseyS <sawlexx@gmail.com>
2022-11-09 13:56:15 +0200

    testDashboardLinkFromMainPage_WhenClick

--- src/test/java/GroupQAGangTest.java
+++ src/test/java/GroupQAGangTest.java
@@ -1,13 +1,10 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
-
 import java.time.Duration;
 
 public class GroupQAGangTest extends BaseTest {
@@ -42,6 +39,5 @@ public class GroupQAGangTest extends BaseTest {
         String actualResult = NameHeader.getText();
 
         Assert.assertEquals(actualResult, expectedResult);
-
     }
 }
