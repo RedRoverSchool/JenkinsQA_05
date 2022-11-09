6a31e21eacabba2cf4d0a82673f34ba1db9802d2
AlekseyS <sawlexx@gmail.com>
2022-11-09 12:49:43 +0200

    testDashboardLinkFromMainPage_WhenClick

--- src/test/java/GroupQAGangTest.java
+++ src/test/java/GroupQAGangTest.java
@@ -1,8 +1,14 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
+
+import java.time.Duration;
 
 public class GroupQAGangTest extends BaseTest {
 
@@ -11,5 +17,31 @@ public class GroupQAGangTest extends BaseTest {
         getDriver().get("https://koma.lux.pl/");
         WebElement link = getDriver().findElement(By.xpath("//*[@id=\"main-nav\"]/ul/li[2]/a"));
         Assert.assertEquals(link.getText(), "Strona główna");
+    }
+
+    @Test
+    public void testDashboardLinkFromMainPage_WhenClick() {
+
+        String url = "https://openweathermap.org/";
+        String expectedResult = "Dashboard";
+
+        getDriver().get(url);
+
+        Boolean dynamicElement = new WebDriverWait(getDriver(), Duration.ofSeconds(60))
+                .until(ExpectedConditions.invisibilityOfElementLocated(
+                        By.xpath("//div[@class ='owm-loader-container']/div[@class='owm-loader']")));
+
+        WebElement dashboardButton = getDriver().findElement(
+                By.xpath("//div[@id='desktop-menu']//li/a[text() = 'Dashboard']"));
+
+        dashboardButton.click();
+
+        WebElement NameHeader = getDriver().findElement(By.xpath("//div/h1/b")
+        );
+
+        String actualResult = NameHeader.getText();
+
+        Assert.assertEquals(actualResult, expectedResult);
+
     }
 }
