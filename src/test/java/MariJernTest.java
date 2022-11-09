6c62ef0c62620b3915fe08bf0694d6253363f8ef
MariJern <marina.jtest@gmail.com>
2022-11-09 14:59:18 -0600

    added new testH2PopUpText to MariJernTest class

--- src/test/java/MariJernTest.java
+++ src/test/java/MariJernTest.java
@@ -4,7 +4,6 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
-
 public class MariJernTest extends BaseTest {
 
     @Test
@@ -15,5 +14,27 @@ public class MariJernTest extends BaseTest {
         WebElement link = getDriver().findElement(By.xpath("//li/a[@href='/buttons']"));
 
         Assert.assertEquals(link.getText(), "Buttons");
+    }
+
+    @Test
+    public void testH2PopUpText_WhenClickingCookiesButton() throws InterruptedException {
+        getDriver().get("https://rus.delfi.lv/");
+
+        Thread.sleep(1000);
+
+        WebElement otherCookiesButton = getDriver().findElement(
+                By.xpath("//div[@id='qc-cmp2-ui'] //button[@mode='secondary']")
+        );
+
+        otherCookiesButton.click();
+
+        WebElement popupH2Text = getDriver().findElement(
+                By.xpath("//div[@id ='qc-cmp2-ui']//h2")
+        );
+
+        Assert.assertEquals(popupH2Text
+                .getTagName(),"h2");
+        Assert.assertEquals(popupH2Text
+                .getText(),"Мы с уважением относимся к вашей конфиденциальности");
     }
 }
