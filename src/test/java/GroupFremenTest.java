22187ad24b4f271b87f3eb1f7ba634714e8ea684
maksymiasko <maksymiasko@gmail.com>
2022-11-10 22:45:39 +0000

    testMICheckModalTitleMessage  added

--- src/test/java/GroupFremenTest.java
+++ src/test/java/GroupFremenTest.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
-
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
