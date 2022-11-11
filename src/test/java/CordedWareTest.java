a5f809c3f7d26db217f0a4d21197974cc05a0c24
Ivan Tretyakov <ivtretyakov56@gmail.com>
2022-11-11 12:28:23 +0500

    testTittleAndUrlWhenGuideMenu_HappyPath_Test() & testTemperatureForCity_HappyPath_Test() added + refactoring

--- src/test/java/CordedWareTest.java
+++ src/test/java/CordedWareTest.java
@@ -1,61 +1,70 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
 public class CordedWareTest extends BaseTest {
 
-    @Ignore
     @Test
-    public void testTemperatureForCity_HappyPath_Test() throws InterruptedException {
-
-        System.setProperty("webdriver.chrome.driver", "/home/cordedware/IdeaProjects/chromedriver");
-
-        WebDriver driver = new ChromeDriver();
-
-        driver.manage().window().maximize();
+    public void testTittleAndUrlWhenGuideMenu_HappyPath_Test() throws InterruptedException{
 
         // AAA
         // Arrange
         String url = "https://openweathermap.org/";
+        String expectedResultUrl = "https://openweathermap.org/guide";
+        String expectedResultTitle = "OpenWeatherMap API guide - OpenWeatherMap";
 
+        // Act
+        getDriver().get(url);
+        Thread.sleep(10000);
+
+        WebElement selectButtonMenu = getDriver().findElement(
+                By.xpath("//a[@href = '/guide']")
+        );
+        selectButtonMenu.click();
+        Thread.sleep(10000);
+
+        String actualResultUrl = getDriver().getCurrentUrl();
+        String actualResultTitle = getDriver().getTitle();
+
+        // Assert
+        Assert.assertEquals(actualResultUrl, expectedResultUrl);
+        Assert.assertEquals(actualResultTitle, expectedResultTitle);
+
+        getDriver().quit();
+    }
+
+    @Test
+    public void testTemperatureForCity_HappyPath_Test() throws InterruptedException {
+
+        // AAA
+        // Arrange
+        String url = "https://openweathermap.org/";
+        String fSymbol = "°F";
         String expectedResult = "°F";
 
         // Act
-        driver.get(url); // 1.
-        Thread.sleep(6000);
+        getDriver().get(url);
+        Thread.sleep(10000);
 
-        WebElement searchButtonWithTemperature = driver.findElement( // 2.
-                By.xpath("//div[@class = 'switch-container']//div[3][@class = 'option']")
+        WebElement searchButtonWithTemperature = getDriver().findElement(
+                By.xpath("//div[3][@class = 'option']")
         );
         searchButtonWithTemperature.click();
-        Thread.sleep(6000);
+        Thread.sleep(10000);
 
-        WebElement checkTemperatureHeader = driver.findElement( // 3.
+        WebElement checkTemperatureHeader = getDriver().findElement(
                 By.xpath("//div[@class = 'current-container mobile-padding']/div[2]/div/span[@class = 'heading']")
         );
         checkTemperatureHeader.click();
-        Thread.sleep(3000);
-        String actualResult = checkTemperatureHeader.getText().substring(2); // .substring(2) находит только "°F" удаля 2 символа вначале
+        Thread.sleep(10000);
+
+        String actualResult = checkTemperatureHeader.getText().substring(checkTemperatureHeader.getText().length()-2);
 
         // Assert
         Assert.assertEquals(actualResult, expectedResult);
 
-
-        driver.quit();
-    }
-
-    @Test
-    public void testHerokuApp() {
-        getDriver().get("https:/formy-project.herokuapp.com/");
-
-        WebElement link = getDriver().findElement(
-                By.xpath("//li/a[@href='/autocomplete']")
-        );
-        Assert.assertEquals(link.getText(), "Autocomplete");
+        getDriver().quit();
     }
 }
