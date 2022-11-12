a108b0abefd3371ebc2b7e8a1d557e0449b68fd2
Karina Kulakova <karina.kulakova83@gmail.com>
2022-11-12 18:17:13 +0200

    add updated test to the GroupCubsTest

--- src/test/java/GroupCubsTest.java
+++ src/test/java/GroupCubsTest.java
@@ -111,10 +111,8 @@ public class GroupCubsTest extends BaseTest {
         getDriver().get("https://www.saucedemo.com/");
         WebElement login = getDriver().findElement(By.xpath("//div[@class='form_group']/input"));
         login.sendKeys("standard_user");
-        Thread.sleep(1000);
         WebElement password = getDriver().findElement(By.id("password"));
         password.sendKeys("secret_sauce");
-        Thread.sleep(1000);
         WebElement loginButton = getDriver().findElement(By.name("login-button"));
         loginButton.click();
     }
