225117279eb565978fa5c0c94de2e410e23f8a00
olpolezhaeva <olpolezhaeva2014@gmail.com>
2022-11-15 17:56:15 +0300

    refactoring

--- src/test/java/PipelineTest.java
+++ src/test/java/PipelineTest.java
@@ -1,13 +1,8 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
-import java.time.Duration;
 
 public class PipelineTest extends BaseTest {
 
