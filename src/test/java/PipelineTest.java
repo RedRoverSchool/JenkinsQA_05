33a60ad0ef1f2504a0f08fe35cedb392d94610e3
olpolezhaeva <olpolezhaeva2014@gmail.com>
2022-11-15 16:34:05 +0300

    Created test_CreatePipelineValidNameApplyDefaultConfigure()

---
+++ src/test/java/PipelineTest.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+import java.time.Duration;
+
+public class PipelineTest extends BaseTest {
+
+    @Test
+    public void test_CreatePipelineValidNameApplyDefaultConfigure() {
+
+        getDriver().findElement(By.linkText("New Item")).click();
+        getDriver().findElement(By.id("name")).sendKeys("Job12");
+        getDriver().findElement(By.className("org_jenkinsci_plugins_workflow_job_WorkflowJob")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+        getDriver().findElement(By.id("yui-gen5-button")).click();
+
+        Assert.assertEquals(
+                new WebDriverWait(getDriver(), Duration.ofSeconds(5)).until(ExpectedConditions.visibilityOf(getDriver().findElement(By.id("notification-bar")))).getText(),
+                "Saved");
+    }
+}
