c5269c7c419ee2754eb58f03a2a063313863d201
Aleksei Chapaev <alekseiachapaev@gmail.com>
2022-11-16 19:30:35 +0100

    refactoring testRenameFreestyleProject, add getRandomName method

--- src/test/java/FreestyleProjectTest.java
+++ src/test/java/FreestyleProjectTest.java
@@ -17,8 +17,12 @@ public class FreestyleProjectTest extends BaseTest {
     private WebDriverWait wait;
     private Actions action;
 
-    private final String freestyleName = RandomStringUtils.randomAlphanumeric(10);
+    private static final String FREESTYLE_NAME = RandomStringUtils.randomAlphanumeric(10);
 
+    private String getRandomName(){
+
+        return RandomStringUtils.randomAlphanumeric(10);
+    }
     private WebDriverWait getWait() {
         if (wait == null) {
             wait = new WebDriverWait(getDriver(), Duration.ofSeconds(20));
@@ -58,37 +62,47 @@ public class FreestyleProjectTest extends BaseTest {
     public void testCreateNewFreestyleProject() {
         getWait().until(ExpectedConditions.elementToBeClickable(By.linkText("New Item"))).click();
 
-        final WebElement itemNameField = getDriver().findElement(By.id("name"));
+        WebElement itemNameField = getDriver().findElement(By.id("name"));
         itemNameField.click();
-        itemNameField.sendKeys(freestyleName);
+        itemNameField.sendKeys(FREESTYLE_NAME);
         getDriver().findElement(By.cssSelector(".hudson_model_FreeStyleProject")).click();
         getDriver().findElement(By.cssSelector("#ok-button")).click();
         clickSubmitButton();
 
-        Assert.assertEquals(getDriver().findElement(By.xpath("//h1")).getText(), "Project " + freestyleName);
+        Assert.assertEquals(getDriver().findElement(By.xpath("//h1")).getText(), "Project " + FREESTYLE_NAME);
     }
 
     @Test(dependsOnMethods = "testCreateNewFreestyleProject")
     public void testPresentationNewProjectOnDashboard() {
         goToDashBoard();
 
-        Assert.assertTrue(getListExistingFreestyleProjectsNames().contains(freestyleName));
+        Assert.assertTrue(getListExistingFreestyleProjectsNames().contains(FREESTYLE_NAME));
     }
 
-    @Test(dependsOnMethods = "testCreateNewFreestyleProject")
+    @Test
     public void testRenameFreestyleProject() {
-        final String newFreestyleName = RandomStringUtils.randomAlphanumeric(10);
+        final String freestyleProjectName = getRandomName();
+        final String newFreestyleProjectName = getRandomName();
+
+        getDriver().findElement(By.linkText("New Item")).click();
+
+        WebElement itemNameField = getDriver().findElement(By.id("name"));
+        itemNameField.click();
+        itemNameField.sendKeys(freestyleProjectName);
+        getDriver().findElement(By.cssSelector(".hudson_model_FreeStyleProject")).click();
+        getDriver().findElement(By.cssSelector("#ok-button")).click();
+        clickSubmitButton();
 
         goToDashBoard();
-        getDriver().findElement(By.linkText(freestyleName)).click();
+        getDriver().findElement(By.linkText(freestyleProjectName)).click();
         getDriver().findElement(By.xpath("//a[@title = 'Rename']")).click();
 
-        final WebElement newNameRow = getDriver().findElement(By.xpath("//input[@name = 'newName']"));
-        getAction().moveToElement(newNameRow).doubleClick().sendKeys(newFreestyleName).perform();
+        WebElement newNameRow = getDriver().findElement(By.xpath("//input[@name = 'newName']"));
+        getAction().moveToElement(newNameRow).doubleClick().sendKeys(newFreestyleProjectName).perform();
         clickSubmitButton();
         goToDashBoard();
 
-        Assert.assertFalse(getListExistingFreestyleProjectsNames().contains(freestyleName));
-        Assert.assertTrue(getListExistingFreestyleProjectsNames().contains(newFreestyleName));
+        Assert.assertFalse(getListExistingFreestyleProjectsNames().contains(freestyleProjectName));
+        Assert.assertTrue(getListExistingFreestyleProjectsNames().contains(newFreestyleProjectName));
     }
 }
