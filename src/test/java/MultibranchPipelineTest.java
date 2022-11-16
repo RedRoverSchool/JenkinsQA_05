0f8fd396190e1cb93847f712b3b6b7a2802c4d26
Igor K <reinar4747@gmail.com>
2022-11-16 17:33:13 +0300

    add MultibranchPipelineTest class

--- src/test/java/MultibranchPipelineTest.java
+++ src/test/java/MultibranchPipelineTest.java
@@ -1,23 +1,15 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
-public class GroupJavanistyTest extends BaseTest {
+public class MultibranchPipelineTest extends BaseTest {
     private final String NEW_ITEM_XPATH = "//div [@class='task '][1]";
     private final String ENTER_AN_ITEM_NAME_XPATH = "//input[@id='name']";
-    private final String FREESTYLE_PROJECT_XPATH = "//li[@class='hudson_model_FreeStyleProject active']";
-    private final String PIPELINE_XPATH = "//li[@class='org_jenkinsci_plugins_workflow_job_WorkflowJob']";
-    private final String MULTI_CONFIGURATION_PROJECT_XPATH = "//li[@class='hudson_matrix_MatrixProject']";
-    private final String FOLDER_XPATH = "//li[@class='com_cloudbees_hudson_plugins_folder_Folder']";
     private final String MULTIBRANCH_PIPELINE_XPATH =
             "//li[@class='org_jenkinsci_plugins_workflow_multibranch_WorkflowMultiBranchProject']";
-    private final String ORGANIZATION_FOLDER_XPATH = "//li[@class='jenkins_branch_OrganizationFolder']";
     private final String BUTTON_OK_XPATH = "//span [@class='yui-button primary large-button']";
     private final String DASHBOARD_XPATH = "//li[@class='item'][1]/a [@class='model-link']";
-
 
     private void buttonClickXpath(String locator) {
         getDriver().findElement(By.xpath(locator)).click();
@@ -39,19 +31,6 @@ public class GroupJavanistyTest extends BaseTest {
         Assert.assertEquals(getDriver().findElement(By.xpath(locator)).getText(), text);
     }
 
-    private void scrollDown(String locator) {
-        JavascriptExecutor js = (JavascriptExecutor) getDriver();
-        WebElement Element = getDriver().findElement(By.xpath(locator));
-        js.executeScript("arguments[0].scrollIntoView();", Element);
-    }
-
-    private void threadSleep1000() throws InterruptedException {
-        Thread.sleep(1000);
-    }
-
-    private void threadSleep500() throws InterruptedException {
-        Thread.sleep(500);
-    }
 
     private void deleteItem(String nameOfItem) {
         getDriver().get("http://localhost:8080/job/" + nameOfItem + "/delete");
@@ -59,7 +38,7 @@ public class GroupJavanistyTest extends BaseTest {
     }
 
     @Test
-    public void Create_Multibranch_pipeline() throws InterruptedException {
+    public void Create_Multibranch_pipeline() {
         String nameOfItem = "MultibranchPipeline";
         buttonClickXpath(NEW_ITEM_XPATH);
         inputTextByXPath(ENTER_AN_ITEM_NAME_XPATH, nameOfItem);
@@ -76,6 +55,5 @@ public class GroupJavanistyTest extends BaseTest {
 
         deleteItem(nameOfItem);
     }
-
 
 }
