6809ee29657c39175d02d0c368b63821d1c68176
Igor K <reinar4747@gmail.com>
2022-11-17 12:53:58 +0300

    Create_Multibranch_Pipeline_Invalid_Name

--- src/test/java/MultibranchPipelineTest.java
+++ src/test/java/MultibranchPipelineTest.java
@@ -10,6 +10,9 @@ public class MultibranchPipelineTest extends BaseTest {
             "//li[@class='org_jenkinsci_plugins_workflow_multibranch_WorkflowMultiBranchProject']";
     private final String BUTTON_OK_XPATH = "//span [@class='yui-button primary large-button']";
     private final String DASHBOARD_XPATH = "//li[@class='item'][1]/a [@class='model-link']";
+    private final String JENKINS_HOME_LINK_CSS = "#jenkins-home-link";
+
+
 
     private void buttonClickXpath(String locator) {
         getDriver().findElement(By.xpath(locator)).click();
@@ -38,7 +41,7 @@ public class MultibranchPipelineTest extends BaseTest {
     }
 
     @Test
-    public void Create_Multibranch_pipeline() {
+    public void Create_Multibranch_Pipeline() {
         String nameOfItem = "MultibranchPipeline";
         buttonClickXpath(NEW_ITEM_XPATH);
         inputTextByXPath(ENTER_AN_ITEM_NAME_XPATH, nameOfItem);
@@ -55,5 +58,30 @@ public class MultibranchPipelineTest extends BaseTest {
 
         deleteItem(nameOfItem);
     }
+    @Test
+    public void Create_Multibranch_Pipeline_Invalid_Name() {
+        buttonClickXpath(NEW_ITEM_XPATH);
+        inputTextByXPath(ENTER_AN_ITEM_NAME_XPATH, "MultibranchPipeline@");
+        buttonClickXpath(MULTIBRANCH_PIPELINE_XPATH);
 
-}
+        Assert.assertEquals((getDriver().findElement(By.cssSelector("#itemname-invalid")).getText()),
+                "» ‘@’ is an unsafe character");
+
+        buttonClickXpath(BUTTON_OK_XPATH);
+
+        Assert.assertEquals(getDriver().getCurrentUrl(), "http://localhost:8080/view/all/createItem");
+        Assert.assertEquals(getDriver().findElement(By.xpath("//div[@id='main-panel']/h1")).getText(),
+                "Error");
+        Assert.assertEquals(getDriver().findElement(By.xpath("//div[@id='main-panel']/p")).getText(),
+                "‘@’ is an unsafe character");
+
+        getDriver().findElement(By.cssSelector(JENKINS_HOME_LINK_CSS)).click();
+
+        getDriver().findElement(By.xpath("//div [@class='empty-state-block']")).isDisplayed();
+    }
+
+
+
+
+
+    }
