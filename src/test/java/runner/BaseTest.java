9b3f5f1ca825ccf7df9a0b206f33fd1c04ee06e3
Vadym Koroliuk <vkqa@outlook.com>
2022-11-18 03:15:19 -0800

    refactoring

--- src/test/java/runner/BaseTest.java
+++ src/test/java/runner/BaseTest.java
@@ -1,15 +1,23 @@
 package runner;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.ITestResult;
 import org.testng.annotations.*;
 import org.testng.annotations.AfterMethod;
 
 import java.lang.reflect.Method;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public abstract class BaseTest {
 
     private WebDriver driver;
+    private WebDriverWait wait;
 
     @BeforeMethod
     protected void beforeMethod(Method method) {
@@ -29,5 +37,20 @@ public abstract class BaseTest {
 
     protected WebDriver getDriver() {
         return driver;
+    }
+
+    protected WebDriverWait getWait(int sec) {
+        if (wait == null) {
+            wait = new WebDriverWait(getDriver(), Duration.ofSeconds(sec));
+        }
+        return wait;
+    }
+
+    protected List<String> getListNames(By by) {
+        List<WebElement> existingJobs =
+                getDriver().findElements(by);
+        List<String> existingJobsNames = existingJobs.stream().map(WebElement::getText).collect(Collectors.toList());
+
+        return existingJobsNames;
     }
 }
