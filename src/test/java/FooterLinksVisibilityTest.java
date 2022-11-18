3199632848cdb27202852f86de39af45e9e59066
BabintsevaS <sb.babintseva@gmail.com>
2022-11-17 19:10:19 -0800

    edited tests

--- src/test/java/FooterLinksVisibilityTest.java
+++ src/test/java/FooterLinksVisibilityTest.java
@@ -3,19 +3,21 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
+import java.util.ArrayList;
+
 public class FooterLinksVisibilityTest extends BaseTest {
 
     private static final By REST_API_LINK = By.xpath("//*[@id='jenkins']/footer/div/div/div[2]/a");
     private static final By JENKINS_LINK = By.xpath("//*[@id='jenkins']/footer/div/div/div[3]/a");
 
     @Test
-    public void testFooterLinkRestIsDisplayed(){
+    public void testFooterLinkRestIsDisplayed() {
 
         Assert.assertTrue(getDriver().findElement(REST_API_LINK).isDisplayed());
     }
 
     @Test
-    public void testFooterLinkRestRedirectToPage(){
+    public void testFooterLinkRestRedirectToPage() {
 
         getDriver().findElement(REST_API_LINK).click();
 
@@ -24,17 +26,23 @@ public class FooterLinksVisibilityTest extends BaseTest {
     }
 
     @Test
-    public void testFooterLinkJenkinsIdDisplayed(){
+    public void testFooterLinkJenkinsIdDisplayed() {
 
         Assert.assertTrue(getDriver().findElement(JENKINS_LINK).isDisplayed());
     }
 
     @Test
-    public void testFooterLinkJenkinsRedirectToPage(){
+    public void testFooterLinkJenkinsRedirectToPage() {
 
         getDriver().findElement(JENKINS_LINK).click();
 
+        ArrayList<String> tabs= new ArrayList<>(getDriver().getWindowHandles());
+        getDriver().switchTo().window(tabs.get(1));
+
         Assert.assertTrue(getDriver().getCurrentUrl().contains("jenkins"));
-        Assert.assertEquals(getDriver().findElement(By.xpath("/html/body/div[2]/div[2]/div[2]/div[3]/h1/span")).getText(), "Jenkins");
+        Assert.assertEquals(getDriver().findElement(By.xpath("//*[@id='ji-toolbar']/a")).getText(), "Jenkins");
+
+        getDriver().switchTo().window(tabs.get(0));
     }
+
 }
