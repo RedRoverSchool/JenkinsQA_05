3852a9d98e6478292768c84760dadcecd45e99db
iKrlkv <il9.korolkov@gmail.com>
2022-11-19 16:23:47 +0500

    RF AddFolderDescriptionTest > testAddFolderDescriptionDuringCreatingNewFolderProcess

--- src/test/java/AddFolderDescriptionTest.java
+++ src/test/java/AddFolderDescriptionTest.java
@@ -8,13 +8,18 @@ public class AddFolderDescriptionTest extends BaseTest {
     private static final String FOLDERNAME = "SomeFolderName";
     private static final String DESCRIPTION = "Some random folder description";
 
-    @Test
-    public void testAddFolderDescription() {
-
+    public void createNewFolder() {
         getDriver().findElement(By.xpath("//div[@id='tasks']//a")).click();
         getDriver().findElement(By.xpath("//input[@name='name']")).sendKeys(FOLDERNAME);
         getDriver().findElement(By.xpath("//div[@id='j-add-item-type-nested-projects']//li")).click();
         getDriver().findElement(By.id("ok-button")).click();
+    }
+
+    @Test
+    public void testAddFolderDescriptionDuringCreatingNewFolderProcess() {
+
+        createNewFolder();
+
         getDriver().findElement(By.tagName("textarea")).sendKeys(DESCRIPTION);
         getDriver().findElement(By.id("yui-gen6-button")).click();
         getDriver().findElement(By.xpath("//ul[@id='breadcrumbs']//li")).click();
