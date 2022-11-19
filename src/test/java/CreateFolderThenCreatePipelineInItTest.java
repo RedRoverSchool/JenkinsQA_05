eb5e734dea9a759a07c1f60cbe6d5a83d0f4d24d
Baha Sadyr <baha.sadyr777@gmail.com>
2022-11-19 13:56:37 -0600

    refactored xpath

--- src/test/java/CreateFolderThenCreatePipelineInItTest.java
+++ src/test/java/CreateFolderThenCreatePipelineInItTest.java
@@ -39,8 +39,8 @@ public class CreateFolderThenCreatePipelineInItTest extends BaseTest {
         while (i <= numberOfJobs2Run) {
             getDriver().findElement(By.xpath("//div[@id='tasks']/div[4]/span/a")).click();
             Thread.sleep(5000);
-           // wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@id='buildHistory']/div[2]/table/tbody/tr/td/div[2]/a")));
-            wait.until(ExpectedConditions.attributeContains(By.xpath("//*[@id='buildHistory']/div[2]/table/tbody/tr/td/div/div/a"),"href", String.valueOf(i)));
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='buildHistory']/div[2]/table/tbody/tr/td/div/div/following-sibling::a[contains(text(),'"+i+"')]")));
+            //wait.until(ExpectedConditions.attributeContains(By.xpath("//*[@id='buildHistory']/div[2]/table/tbody/tr/td/div/div/a"),"href", String.valueOf(i)));
             i++;
         }
         List<WebElement> listWithTr = getDriver().findElements(By.xpath("//div[@id='buildHistory']/div[2]/table/tbody/tr/td/div/div/a"));
