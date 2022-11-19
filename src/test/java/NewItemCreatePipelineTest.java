37b4d9b81bdc9bcf2aff55333048bac1d50c188e
Igor klimenko <103316820+patriotby07@users.noreply.github.com>
2022-11-19 18:38:11 +0300

    AT 00.02.07 | New Item > Create Pipeline | Create from existing job select not existing name

--- src/test/java/NewItemCreatePipelineTest.java
+++ src/test/java/NewItemCreatePipelineTest.java
@@ -1,11 +1,17 @@
+import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
+import java.time.Duration;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -13,6 +19,28 @@ public class NewItemCreatePipelineTest extends BaseTest {
 
     private void click(By by) {
         getDriver().findElement(by).click();
+    }
+
+    private static String getRandomStr() {
+        return RandomStringUtils.random(7, true,true);
+    }
+
+    private void createPipeline(String jobName) {
+        setJobPipeline(jobName);
+        getDriver().findElement(By.id("ok-button")).click();
+    }
+
+    private void setJobPipeline(String jobName) {
+        getDriver().findElement(By.linkText("New Item")).click();
+        getDriver().findElement(By.id("name")).sendKeys(jobName);
+        getDriver().findElement(By.xpath("//span[text()='Pipeline']")).click();
+    }
+
+    private JavascriptExecutor scrollPageDown() {
+        JavascriptExecutor js = (JavascriptExecutor) getDriver();
+        js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+
+        return js;
     }
 
     @Test
@@ -84,5 +112,19 @@ public class NewItemCreatePipelineTest extends BaseTest {
 
         Assert.assertTrue(getDriver().findElement(By.className("jenkins-breadcrumbs"))
                 .getAttribute("textContent").contains(itemName));
+    }
+
+    @Test
+    public void testCreateNewItemFromOtherNonExistingName() {
+        final String jobName = getRandomStr();
+
+        setJobPipeline(jobName);
+        scrollPageDown();
+        new Actions(getDriver()).pause(1500).moveToElement(getDriver().findElement(By.id("from"))).click()
+                .sendKeys(jobName).perform();
+        getDriver().findElement(By.id("ok-button")).click();
+
+        Assert.assertEquals(getDriver().findElement(By.xpath("//div[@id='main-panel']/p")).getText(),
+                "No such job: " + jobName);
     }
 }
