62ffeca78852c7964e6456d9e1da191e3719c1b6
Gena Shepel <genashepel@gmail.com>
2022-11-20 12:30:08 -0500

    Delete Pipeline Project

---
+++ src/test/java/DeletePipelineProjectTest.java
@@ -0,0 +1,28 @@
+import org.apache.commons.lang3.RandomStringUtils;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+public class DeletePipelineProjectTest extends BaseTest {
+    private static final String PIPELINE_NAME = RandomStringUtils.randomAlphanumeric(10);
+
+    @Test
+    public void testDeletePipelineProject() throws InterruptedException {
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.id("name")).sendKeys(PIPELINE_NAME);
+        Thread.sleep(1000);
+        getDriver().findElement(
+                By.xpath("//span[contains(@class, 'label') and text() = 'Pipeline']")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+        getDriver().findElement(By.id("yui-gen6-button")).click();
+        getDriver().findElement(By.id("jenkins-home-link")).click();
+        getDriver().findElement(By.xpath("//a[@href='job/" + PIPELINE_NAME + "/']")).click();
+        getDriver().findElement(By.xpath("//span[text() = 'Delete Pipeline']")).click();
+        getDriver().switchTo().alert().accept();
+
+        Assert.assertEquals(getDriver().findElement(
+                By.xpath("//h1[text() = 'Welcome to Jenkins!']")).getText(), "Welcome to Jenkins!");
+
+    }
+}
