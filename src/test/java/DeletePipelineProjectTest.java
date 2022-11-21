1010fcb0d280e63ff53807f288577ad82f148f9e
Gena Shepel <genashepel@gmail.com>
2022-11-20 21:37:17 -0500

    Next change

--- src/test/java/DeletePipelineProjectTest.java
+++ src/test/java/DeletePipelineProjectTest.java
@@ -1,14 +1,19 @@
 import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
+import java.time.Duration;
+
 public class DeletePipelineProjectTest extends BaseTest {
     private static final String PIPELINE_NAME = RandomStringUtils.randomAlphanumeric(10);
-
+    private static final By TEXT_WELCOME = By.cssSelector("div#main-panel .empty-state-block h1");
     @Test
-    public void testDeletePipelineProject() throws InterruptedException {
+    public void testDeletePipelineProject() {
         getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
         getDriver().findElement(By.id("name")).sendKeys(PIPELINE_NAME);
         getDriver().findElement(
@@ -19,8 +24,11 @@ public class DeletePipelineProjectTest extends BaseTest {
         getDriver().findElement(By.xpath("//a[@href='job/" + PIPELINE_NAME + "/']")).click();
         getDriver().findElement(By.xpath("//span[text() = 'Delete Pipeline']")).click();
         getDriver().switchTo().alert().accept();
-        String actualResult = getDriver().findElement(
-                By.cssSelector("div#main-panel .empty-state-block h1")).getText();
+
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(TEXT_WELCOME));
+
+        String actualResult = getDriver().findElement(TEXT_WELCOME).getText();
 
         Assert.assertEquals(actualResult, "Welcome to Jenkins!");
     }
