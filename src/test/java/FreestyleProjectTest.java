43a9cc18d8d8b1243ef47d1a2afb18cf8d9dc971
ServachakM <servachak.m.u@gmail.com>
2022-11-22 01:19:26 +0200

    AT 01.01.01 | Freestyle Project > View Project Page > Verify that the freestyle project page is opening from the dashboard list

--- src/test/java/FreestyleProjectTest.java
+++ src/test/java/FreestyleProjectTest.java
@@ -168,7 +168,6 @@ public class FreestyleProjectTest extends BaseTest {
 
         Assert.assertEquals(actualChangesText, "No builds.");
     }
-}
 
     @Test
     public void testFreestyleProjectPageIsOpenedFromDashboard() {
