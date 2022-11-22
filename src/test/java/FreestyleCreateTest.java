165a400c19e7b725fe47ef8833f84c72e20c2047
Walter Bashinski <wbashinski@paymentus.com>
2022-11-21 21:28:42 -0500

    AT 00.01.07 New item > Freestyle Project> Create Freestyle Project

--- src/test/java/FreestyleCreateTest.java
+++ src/test/java/FreestyleCreateTest.java
@@ -8,18 +8,19 @@ public class FreestyleCreateTest extends BaseTest {
     String x = UUID.randomUUID().toString().substring(0, 2);
     private final String name = "project-" + x;
 
-    private static final By NEW_ITEM = By.xpath("//a[@href='/view/all/newJob']");
-    private static final By ITEM_NAME = By.xpath("//input[@id='name']");
-    private static final By PROJECT_TYPE = By.xpath("//span[normalize-space()='Freestyle project']");
-    private static final By OK_BUTTON = By.cssSelector(".yui-button.primary.large-button");
-    private static final By SAVE_BUTTON = By.xpath("//*[@id='yui-gen23']");
-    private static final By DASHBOARD = By.xpath("//a[normalize-space()='Dashboard']");
-    private static final By JOB_IN = By.xpath("//a[@class='jenkins-table__link model-link inside']");
-    private static final By DELETE_PROJECT = By.xpath("//span[contains(text(),'Delete Project')]");
-    private static final By ACTUAL_RESULT = By.cssSelector(".jenkins-table__link.model-link.inside");
+    private static By NEW_ITEM = By.xpath("//a[@href='/view/all/newJob']");
+    private static By ITEM_NAME = By.xpath("//input[@id='name']");
+    private static By PROJECT_TYPE = By.xpath("//span[normalize-space()='Freestyle project']");
+    private static By OK_BUTTON = By.cssSelector(".yui-button.primary.large-button");
+    private static By SAVE_BUTTON = By.xpath("//*[@id='yui-gen23']");
+    private static By DASHBOARD = By.xpath("//a[normalize-space()='Dashboard']");
+    private static By JOB_IN = By.xpath("//a[@class='jenkins-table__link model-link inside']");
+    private static By DELETE_PROJECT = By.xpath("//span[contains(text(),'Delete Project')]");
+    private static By ACTUAL_RESULT = By.cssSelector(".jenkins-table__link.model-link.inside");
 
     @Test
     public void testCreateFreestyleProject() {
+
         getDriver().findElement(NEW_ITEM).click();
         getDriver().findElement(ITEM_NAME).sendKeys(name);
         getDriver().findElement(PROJECT_TYPE).click();
@@ -33,5 +34,4 @@ public class FreestyleCreateTest extends BaseTest {
         getDriver().findElement(DELETE_PROJECT).click();
         getDriver().switchTo().alert().accept();
     }
-
 }
