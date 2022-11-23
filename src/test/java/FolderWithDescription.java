1ed710855bafb487ea349ee051049ec20b413b01
Walter Bashinski <wbashinski@paymentus.com>
2022-11-23 06:53:44 -0500

    Fixed errors

--- src/test/java/FolderWithDescription.java
+++ src/test/java/FolderWithDescription.java
@@ -5,6 +5,7 @@ import runner.BaseTest;
 
 public class FolderWithDescription extends BaseTest {
     private final String DESCRIPTION = "This folder contains job's documentation";
+
     @Test
     public void createFolderDescriptionTest() {
 
@@ -17,10 +18,5 @@ public class FolderWithDescription extends BaseTest {
         getDriver().findElement(By.cssSelector("#yui-gen6-button")).click();
 
         Assert.assertTrue(getDriver().findElement(By.cssSelector("#view-message")).getText().contains(DESCRIPTION));
-
-        getDriver().findElement(By.xpath("//a[normalize-space()='Dashboard']")).click();
-        getDriver().findElement(By.cssSelector("a[class='jenkins-table__link model-link inside'] button[class='jenkins-menu-dropdown-chevron']")).click();
-        getDriver().findElement(By.xpath("(//span[normalize-space()='Delete Folder'])[1]")).click();
-        getDriver().findElement(By.cssSelector("#yui-gen1-button")).click();
     }
 }
