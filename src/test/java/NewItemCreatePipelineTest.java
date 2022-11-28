6d9f004ef2ab3c641cd8754e87c3f109cd953acd
Igor klimenko <103316820+patriotby07@users.noreply.github.com>
2022-11-28 23:02:07 +0300

    AT 09.07.01 | Manage Jenkins > Global Tool Configuration > Add Maven name field. Checking the disappearance of the warning message

--- src/test/java/NewItemCreatePipelineTest.java
+++ src/test/java/NewItemCreatePipelineTest.java
@@ -166,7 +166,7 @@ public class NewItemCreatePipelineTest extends BaseTest {
                 "//a[@href='configureTools']"))).click().perform();
 
         scrollPageDown();
-        new Actions(getDriver()).pause(1500).moveToElement(getDriver().findElement(By.id("yui-gen12-button"))).click().perform();
+        getDriver().findElement(By.id("yui-gen12-button")).click();
         new Actions(getDriver()).moveToElement(getDriver().findElement(By.cssSelector(
                 "input[checkurl$='MavenInstallation/checkName']"))).click().sendKeys("Maven").perform();
         scrollPageDown();
