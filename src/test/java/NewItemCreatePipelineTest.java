735f1cda6a473aea5f1e4f0698f6667c82823d09
Igor klimenko <103316820+patriotby07@users.noreply.github.com>
2022-11-28 22:56:00 +0300

    AT 09.07.01 | Manage Jenkins > Global Tool Configuration > Add Maven name field. Checking the disappearance of the warning message

--- src/test/java/NewItemCreatePipelineTest.java
+++ src/test/java/NewItemCreatePipelineTest.java
@@ -152,7 +152,7 @@ public class NewItemCreatePipelineTest extends BaseTest {
                 .sendKeys(gitHubRepo).perform();
         getDriver().findElement(SAVE_BUTTON).click();
 
-        final WebElement sideMenuGitHub = getDriver().findElement(By.xpath("(//a[contains(@class,'task-link')])[8]"));
+        final WebElement sideMenuGitHub = getDriver().findElement(By.xpath("(//a[contains(@class,'task-link')])[7]"));
 
         Assert.assertTrue(sideMenuGitHub.isDisplayed());
         Assert.assertTrue(sideMenuGitHub.getAttribute("href").contains(gitHubRepo));
