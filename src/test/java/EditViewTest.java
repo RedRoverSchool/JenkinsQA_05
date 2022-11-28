94f6167455040e7bb72c7e224e54f5f7fdfc31a2
SergeDot <sampleemail@yaho2.com>
2022-11-28 18:24:35 -0500

    AT 11.03.03 | New view > Edit View > Add Both Filters to Global View resolving failing tests

--- src/test/java/EditViewTest.java
+++ src/test/java/EditViewTest.java
@@ -65,7 +65,7 @@ public class EditViewTest extends BaseTest{
     }
     public void deleteAllViews(){
         getDriver().findElement(MY_VIEWS_XP).click();
-        List<WebElement> allViews = getDriver().findElements(By.xpath("//div[@class='tab']/a[contains(@href,'my-views/view')]')]"));
+        List<WebElement> allViews = getDriver().findElements(By.xpath("//div[@class='tab']/a[contains(@href, 'my-views/view')]"));
         System.out.println(getUserName());
 //        if (allViews.size() > 0) {
 //            getDriver().findElement(By.xpath("//div[@class='tab']/a[contains(@href,'my-views/view')]')]")).click();
