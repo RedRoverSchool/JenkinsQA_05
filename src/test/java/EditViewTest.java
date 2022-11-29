be30fa741288769c53ee4b227345de854337974c
SergeDot <sampleemail@yaho2.com>
2022-11-28 19:35:11 -0500

    AT 11.03.03 | New view > Edit View > Add Both Filters to Global View resolving failing tests

--- src/test/java/EditViewTest.java
+++ src/test/java/EditViewTest.java
@@ -1,5 +1,4 @@
 import org.openqa.selenium.*;
-import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
@@ -16,18 +15,13 @@ public class EditViewTest extends BaseTest{
     private static final By INPUT_NAME_ID = By.id("name");
     private static final By DELETE_VIEW_CSS = By.cssSelector("a[href='delete']");
 
-    public String getUserName() {
-        String url = getDriver().findElement(By
-                        .xpath("//a[@class='model-link' and contains(@href,'/user/')]"))
-                .getAttribute("href");
-        return url.substring(6,url.length()-1);
-    }
+
     private void createItem(int i){
-        final By CSS_FREESTYLE_0 = By.cssSelector(".j-item-options .hudson_model_FreeStyleProject");//
-        final By CSS_PIPELINE_1 = By.cssSelector(".j-item-options .org_jenkinsci_plugins_workflow_job_WorkflowJob");//
-        final By CSS_MULTICONFIG_2 = By.cssSelector(".j-item-options .hudson_matrix_MatrixProject");//
-        final By CSS_FOLDER_3 = By.cssSelector(".j-item-options .com_cloudbees_hudson_plugins_folder_Folder");//
-        final By CSS_MULTIBRANCH_4 = By.cssSelector(".j-item-options .org_jenkinsci_plugins_workflow_multibranch_WorkflowMultiBranchProject");//
+        final By CSS_FREESTYLE_0 = By.cssSelector(".j-item-options .hudson_model_FreeStyleProject");
+        final By CSS_PIPELINE_1 = By.cssSelector(".j-item-options .org_jenkinsci_plugins_workflow_job_WorkflowJob");
+        final By CSS_MULTICONFIG_2 = By.cssSelector(".j-item-options .hudson_matrix_MatrixProject");
+        final By CSS_FOLDER_3 = By.cssSelector(".j-item-options .com_cloudbees_hudson_plugins_folder_Folder");
+        final By CSS_MULTIBRANCH_4 = By.cssSelector(".j-item-options .org_jenkinsci_plugins_workflow_multibranch_WorkflowMultiBranchProject");
         final By CSS_ORGFOLDER_5 = By.cssSelector(".j-item-options .jenkins_branch_OrganizationFolder");
         final By[] menuOptions = {CSS_FREESTYLE_0,CSS_PIPELINE_1, CSS_MULTICONFIG_2, CSS_FOLDER_3, CSS_MULTIBRANCH_4, CSS_ORGFOLDER_5};
 
@@ -47,6 +41,7 @@ public class EditViewTest extends BaseTest{
     }
 
     public void createGlobalView() {
+        getDriver().findElement(DASHBOARD_CSS).click();
         getDriver().findElement(MY_VIEWS_XP).click();
         getDriver().findElement(By.cssSelector(".addTab")).click();
         getDriver().findElement(INPUT_NAME_ID).sendKeys(RANDOM_ALPHANUMERIC);
@@ -63,6 +58,7 @@ public class EditViewTest extends BaseTest{
 
     public void globalViewSeriesPreConditions() {
         createManyItems(1);
+        deleteAllViews();
         createGlobalView();
     }
     public void deleteAllViews(){
@@ -74,20 +70,12 @@ public class EditViewTest extends BaseTest{
             getDriver().findElement(SUBMIT_BUTTON_CSS).click();
             allViews = getDriver().findElements(By.xpath("//div[@class='tab']/a[contains(@href, 'my-views/view')]"));
         }
-
-//        for (WebElement element : allViews) {
-//            element.click();
-//            getDriver().findElement(DELETE_VIEW_CSS).click();
-//            getDriver().findElement(SUBMIT_BUTTON_CSS).click();
-//            getDriver().findElement(DASHBOARD_CSS).click();
-//        }
     }
 
     @Test
     public void testGlobalViewAddFilterBuildQueue() {
-        createManyItems(1);
-        deleteAllViews();
         globalViewSeriesPreConditions();
+
         getDriver().findElement(FILTER_QUEUE_CSS).click();
         getDriver().findElement(SUBMIT_BUTTON_CSS).click();
         boolean newPaneIsDisplayed = getDriver().findElements(By.cssSelector(".pane-header-title"))
@@ -99,8 +87,6 @@ public class EditViewTest extends BaseTest{
 
     @Test
     public void testGlobalViewAddBothFilters() {
-        createManyItems(1);
-        deleteAllViews();
         globalViewSeriesPreConditions();
 
         getDriver().findElement(FILTER_QUEUE_CSS).click();
