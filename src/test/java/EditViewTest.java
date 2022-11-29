ae3b216e9b5e6424eed4b9175a67469c1fbb366a
SergeDot <sampleemail@yaho2.com>
2022-11-29 03:21:44 -0500

    AT 11.03.10 | New view > Edit View > Add Filter Build Queue to List View  removed unnecessary line

--- src/test/java/EditViewTest.java
+++ src/test/java/EditViewTest.java
@@ -221,7 +221,6 @@ public class EditViewTest extends BaseTest{
     @Test
     public void testListViewAddFilterBuildQueue() {
         listViewSeriesPreConditions();
-        goToEditView();
 
         getDriver().findElement(FILTER_QUEUE_CSS).click();
         getDriver().findElement(SUBMIT_BUTTON_CSS).click();
