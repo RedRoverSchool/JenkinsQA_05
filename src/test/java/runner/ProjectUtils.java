6270c4c37560a1d8f5a2fbb03158f5de7a3f4529
Vadym Koroliuk <vkqa@outlook.com>
2022-11-30 00:29:22 -0800

    Regactoring method getUrl()

--- src/test/java/runner/ProjectUtils.java
+++ src/test/java/runner/ProjectUtils.java
@@ -3,6 +3,8 @@ package runner;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+import java.util.Objects;
+
 import static runner.BaseUtils.PREFIX_PROP;
 
 public final class ProjectUtils {
@@ -11,12 +13,18 @@ public final class ProjectUtils {
     private static final String PROP_ADMIN_USERNAME = PREFIX_PROP + "admin.username";
     private static final String PROP_ADMIN_PAS = PREFIX_PROP + "admin.password";
 
+    private static String BASE_URL;
+
     public static void get(WebDriver driver) {
         driver.get(getUrl());
     }
 
     static String getUrl() {
-        return String.format("http://localhost:%s/", BaseUtils.getProperties().getProperty(PROP_PORT));
+        if(Objects.equals(BASE_URL, null)){
+            BaseUtils.log("Method getUrl() was invoked");
+            BASE_URL = String.format("http://localhost:%s/", BaseUtils.getProperties().getProperty(PROP_PORT));
+        }
+        return BASE_URL;
     }
 
     static void login(WebDriver driver) {
