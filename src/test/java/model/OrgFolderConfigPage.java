583c78f4e68c7e967b5e0355ecaada0c279565bc
InaChemerko <innachemerko@gmail.com>
2022-12-06 21:10:44 -0700

    added clickSaveButton()

--- src/test/java/model/OrgFolderConfigPage.java
+++ src/test/java/model/OrgFolderConfigPage.java
@@ -1,10 +1,21 @@
 package model;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class OrgFolderConfigPage extends BasePage{
+
+    @FindBy(id = "yui-gen15-button")
+    private WebElement saveButton;
 
     public OrgFolderConfigPage(WebDriver driver) {
         super(driver);
     }
+    public void clickSaveButton(){
+        saveButton.click();
+    }
+
+
 }
