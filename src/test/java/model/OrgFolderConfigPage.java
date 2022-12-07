bc5feefcb9f488798c0510f679dbf50853041f05
InaChemerko <innachemerko@gmail.com>
2022-12-06 20:54:55 -0700

    added OrgFolderConfigPage

---
+++ src/test/java/model/OrgFolderConfigPage.java
@@ -0,0 +1,10 @@
+package model;
+
+import org.openqa.selenium.WebDriver;
+
+public class OrgFolderConfigPage extends BasePage{
+
+    public OrgFolderConfigPage(WebDriver driver) {
+        super(driver);
+    }
+}
