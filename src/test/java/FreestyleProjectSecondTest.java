b4caa5146b864118a2a6ee19943cd7d91fc0253d
Inna Romankova <romankova03071978@gmail.com>
2022-12-07 22:55:51 +0300

    waits added for testCreateWithDescriptionFreestyleProject

--- src/test/java/FreestyleProjectSecondTest.java
+++ src/test/java/FreestyleProjectSecondTest.java
@@ -21,7 +21,9 @@ public class FreestyleProjectSecondTest extends BaseTest {
     public void testCreateFreestyleProject(){
 
         getDriver().findElement(By.xpath("//span/a[@href='/view/all/newJob']")).click();
-        getDriver().findElement(By.xpath("//input[@class='jenkins-input']")).sendKeys(FREESTYLE_NAME);
+        getWait(10).until(ExpectedConditions
+                .visibilityOfElementLocated(By.xpath("//input[@class='jenkins-input']")))
+                .sendKeys(FREESTYLE_NAME);
         getDriver().findElement(By.xpath("//img[@class='icon-freestyle-project icon-xlg']")).click();
         getDriver().findElement(By.id("ok-button")).click();
 
