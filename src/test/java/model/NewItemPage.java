84cab3555206d45fedbc3362d5d0f3f20fd11a38
janarkqa <janarkqa@gmail.com>
2022-12-08 01:25:49 -0600

    refactor testCreateNewFreestyleProject()

--- src/test/java/model/NewItemPage.java
+++ src/test/java/model/NewItemPage.java
@@ -1,6 +1,5 @@
 package model;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -11,14 +10,18 @@ public class NewItemPage extends BasePage {
     @FindBy(id = "name")
     private WebElement itemName;
 
-    @FindBy(className = "com_cloudbees_hudson_plugins_folder_Folder")
-    private WebElement folderType;
-
     @FindBy(className = "btn-decorator")
     private WebElement okButton;
 
+    @FindBy(className = "hudson_model_FreeStyleProject")
+    private WebElement freestyleProject;
+
+    @FindBy(className = "com_cloudbees_hudson_plugins_folder_Folder")
+    private WebElement folderType;
+
     @FindBy(xpath = "//li[@class = 'jenkins_branch_OrganizationFolder']")
     private WebElement orgFolder;
+
 
     public NewItemPage(WebDriver driver) {
         super(driver);
@@ -28,6 +31,13 @@ public class NewItemPage extends BasePage {
         itemName.sendKeys(name);
 
         return this;
+    }
+
+    public FreestyleProjectConfigPage selectFreestyleProjectAndClickOk() {
+        freestyleProject.click();
+        okButton.click();
+
+        return new FreestyleProjectConfigPage(getDriver());
     }
 
     public FolderConfigPage selectFolderAndClickOk() {
