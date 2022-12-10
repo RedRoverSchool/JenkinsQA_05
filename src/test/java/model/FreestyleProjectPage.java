d46deac546982a2946ed247c88e01afacd7a1a13
Inna Romankova <romankova03071978@gmail.com>
2022-12-10 21:38:08 +0300

    Refactored to POM: - testConfigurationProvideDiscardOldBuildsWithDaysToKeepBuilds()

--- src/test/java/model/FreestyleProjectPage.java
+++ src/test/java/model/FreestyleProjectPage.java
@@ -89,4 +89,10 @@ public class FreestyleProjectPage extends BasePage {
 
         return new HomePage(getDriver());
     }
+
+    public FreestyleProjectConfigPage clickSideMenuConfigureLink() {
+        sideMenuConfigure.click();
+
+        return new FreestyleProjectConfigPage(getDriver());
+    }
 }
