8e886a7a3242039caae1eac9b026d140141edf84
Anna Fedorova <snysmymric@gmail.com>
2022-12-12 16:38:10 -0800

    fix ci error, remove import for method getRundom, create ITEM_NEW_DESCRIPTION constant

--- src/test/java/PipelineTest.java
+++ src/test/java/PipelineTest.java
@@ -14,15 +14,14 @@ import runner.BaseTest;
 import runner.ProjectUtils;
 import runner.TestUtils;
 
-import static runner.TestUtils.getRandomStr;
-
 public class PipelineTest extends BaseTest {
     private static final String RENAME_SUFFIX = "renamed";
     private static final String PIPELINE_NAME = generatePipelineProjectName();
-    private static final String pipeline_name = getRandomStr();
+    private static final String pipeline_name = TestUtils.getRandomStr();
     private static final String VIEW_NAME = RandomStringUtils.randomAlphanumeric(5);
-    private static final String RANDOM_STRING  = getRandomStr(7);
+    private static final String RANDOM_STRING  = TestUtils.getRandomStr(7);
     private static final String ITEM_DESCRIPTION = "This is a sample description for item";
+    private static final String ITEM_NEW_DESCRIPTION = "New description";
 
     private static final By NEW_ITEM = By.xpath("//a[@href='/view/all/newJob']");
     private static final By ITEM_NAME = By.id("name");
@@ -120,7 +119,7 @@ public class PipelineTest extends BaseTest {
     @Test
     public void testCreatedPipelineDisplayedOnMyViews() {
 
-        final String pipelineName = getRandomStr(5);
+        final String pipelineName =TestUtils.getRandomStr(5);
 
         MyViewsPage pipelineNameInMyViewList = new HomePage(getDriver())
                 .clickNewItem()
@@ -237,7 +236,7 @@ public class PipelineTest extends BaseTest {
     @Test
     public void testPipelinePreviewDescription() {
 
-        String pipelinePojectName = getRandomStr();
+        String pipelinePojectName =TestUtils.getRandomStr();
         createPipelineProjectCuttedVersion(pipelinePojectName);
 
         getDriver().findElement(TEXTAREA_DESCRIPTION).sendKeys(pipelinePojectName + "description");
@@ -253,7 +252,7 @@ public class PipelineTest extends BaseTest {
     @Test
     public void testPipelineHidePreviewDescription() {
 
-        String pipelinePojectName = getRandomStr();
+        String pipelinePojectName =TestUtils.getRandomStr();
         createPipelineProjectCuttedVersion(pipelinePojectName);
 
         getDriver().findElement(TEXTAREA_DESCRIPTION).sendKeys(pipelinePojectName + "description");
@@ -270,7 +269,7 @@ public class PipelineTest extends BaseTest {
     @Test
     public void testPipelineAEditDescription() {
 
-        String pipelinePojectName = getRandomStr();
+        String pipelinePojectName =TestUtils.getRandomStr();
         createPipelineProjectCuttedVersion(pipelinePojectName);
         getDriver().findElement(TEXTAREA_DESCRIPTION).sendKeys(pipelinePojectName + "description");
         getDriver().findElement(BUTTON_SAVE).click();
@@ -423,9 +422,22 @@ public class PipelineTest extends BaseTest {
                 ITEM_DESCRIPTION);
     }
 
-    @Test (dependsOnMethods = "testCreateNewPipelineWithDescription")
+    @Test(dependsOnMethods = "testCreateNewPipelineWithDescription")
+    public void testEditPipelineDescription()  {
+
+        String actualDescription = new HomePage(getDriver())
+                .clickJobDropDownMenu(RANDOM_STRING)
+                .clickPipelineProjectName()
+                .editDescription(ITEM_NEW_DESCRIPTION)
+                .clickSaveButton()
+                .getDescription();
+
+        Assert.assertEquals(actualDescription, ITEM_NEW_DESCRIPTION);
+    }
+
+    @Test (dependsOnMethods = "testEditPipelineDescription")
     public void testCreateNewPipelineFromExisting() {
-        final String jobName = getRandomStr(7);
+        final String jobName =TestUtils.getRandomStr(7);
 
         String actualJobName = new HomePage(getDriver())
                 .clickNewItem()
@@ -438,20 +450,6 @@ public class PipelineTest extends BaseTest {
         String actualDescription = new PipelineProjectPage(getDriver()).getDescription();
 
         Assert.assertEquals(actualJobName,jobName);
-        Assert.assertEquals(actualDescription,ITEM_DESCRIPTION);
-    }
-
-    @Test(dependsOnMethods = "testCreateNewPipelineWithDescription")
-    public void testEditPipelineDescription()  {
-        final String newDescription = "new description";
-
-        String actualDescription = new HomePage(getDriver())
-                .clickJobDropDownMenu(RANDOM_STRING)
-                .clickPipelineProjectName()
-                .editDescription(newDescription)
-                .clickSaveButton()
-                .getDescription();
-
-        Assert.assertEquals(actualDescription, newDescription);
+        Assert.assertEquals(actualDescription,ITEM_NEW_DESCRIPTION);
     }
 }
