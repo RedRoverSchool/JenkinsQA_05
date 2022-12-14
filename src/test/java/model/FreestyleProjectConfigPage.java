50e5f3152c6c1998d185254ed0fb411069a19619
Inna Romankova <romankova03071978@gmail.com>
2022-12-14 22:03:55 +0300

    Refactoring testVerifyOptionsInBuildStepsSection() to POM

--- src/test/java/model/FreestyleProjectConfigPage.java
+++ src/test/java/model/FreestyleProjectConfigPage.java
@@ -247,10 +247,8 @@ public class FreestyleProjectConfigPage extends BasePage {
         getWait(10).until(TestUtils.ExpectedConditions.elementIsNotMoving(buildStepsButton));
         new Actions(getDriver())
                 .moveToElement(buildStepsButton)
-                .pause(1000)
+                .click()
                 .perform();
-        getWait(10).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".yui-button-hover.yui-menu-button-hover")));
-        getWait(10).until(TestUtils.ExpectedConditions.elementIsNotMoving(buildStepsButton)).click();
         getWait(10).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".yui-button-active.yui-menu-button-active")));
 
         return this;
