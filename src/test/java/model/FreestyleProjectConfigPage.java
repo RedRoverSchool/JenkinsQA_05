97dff2d9925f7d1bb7db65b57b8530aa9e1a23a1
Inna Romankova <romankova03071978@gmail.com>
2022-12-15 13:45:55 +0300

    Refactoring testSelectBuildPeriodicallyCheckbox() to POM

--- src/test/java/model/FreestyleProjectConfigPage.java
+++ src/test/java/model/FreestyleProjectConfigPage.java
@@ -68,7 +68,6 @@ public class FreestyleProjectConfigPage extends BasePage {
     @FindBy(xpath = "//div[@class = 'jenkins-form-item hetero-list-container with-drag-drop  ']/div[3]//input[@name = 'parameter.name']")
     private WebElement fieldInputBooleanParameterName;
 
-
     @FindBy(xpath = "//label[text() = 'Set by Default']")
     private WebElement setByDefault;
 
@@ -86,6 +85,15 @@ public class FreestyleProjectConfigPage extends BasePage {
 
     @FindBy(xpath = "//button[text()='Add build step']/../../..//a[@href='#']")
     private List<WebElement> listOfElementsInBuildStepsDropDown;
+
+    @FindBy(xpath = "//button[@data-section-id='build-triggers']")
+    private WebElement buildTriggersSideMenuOption;
+
+    @FindBy(xpath = "//label[text()='Build periodically']")
+    private WebElement buildPeriodicallyOption;
+
+    @FindBy(name = "hudson-triggers-TimerTrigger")
+    private WebElement buildPeriodicallyCheckbox;
 
     public FreestyleProjectConfigPage(WebDriver driver) {
         super(driver);
@@ -267,5 +275,33 @@ public class FreestyleProjectConfigPage extends BasePage {
                 .stream()
                 .map(WebElement::getText)
                 .collect(Collectors.toCollection(HashSet::new));
+    }
+
+    public FreestyleProjectConfigPage clickBuildTriggersSideMenuOption() {
+        buildTriggersSideMenuOption.click();
+
+        return this;
+    }
+
+    public FreestyleProjectConfigPage scrollAndClickBuildPeriodicallyCheckbox() {
+        scrollToElement_PlaceInCenter(getDriver(), buildPeriodicallyOption);
+        getWait(10).until(TestUtils.ExpectedConditions.elementIsNotMoving(buildPeriodicallyOption)).click();
+        getWait(10).until(ExpectedConditions
+                .elementSelectionStateToBe(By.name("hudson-triggers-TimerTrigger"), true));
+
+        return this;
+    }
+
+    public boolean verifyThatBuildPeriodicallyCheckboxIsSelected() {
+
+        return buildPeriodicallyCheckbox.isSelected();
+    }
+
+    public FreestyleProjectConfigPage uncheckBuildPeriodicallyCheckbox() {
+        if (buildPeriodicallyCheckbox.isSelected()) {
+            getWait(10).until(TestUtils.ExpectedConditions.elementIsNotMoving(buildPeriodicallyOption)).click();
+        }
+
+        return this;
     }
 }
