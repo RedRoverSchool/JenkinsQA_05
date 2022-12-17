9f671785240c05d9428420df2c16cdf4e8e80d44
DESKTOP-MDKATNF\gregg <gregg190@gmail.com>
2022-12-17 23:38:16 +0300

    added packages to project structure

---
+++ src/test/java/model/organization_folder/OrgFolderConfigPage.java
@@ -0,0 +1,22 @@
+package model.organization_folder;
+
+import model.HomePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class OrgFolderConfigPage extends HomePage {
+
+    @FindBy(id = "yui-gen15-button")
+    private WebElement saveButton;
+
+    public OrgFolderConfigPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public OrgFolderStatusPage clickSaveButton() {
+        saveButton.click();
+
+        return new OrgFolderStatusPage(getDriver());
+    }
+}
