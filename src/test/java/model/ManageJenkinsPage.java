eceea1f3b5cc5c132744e2f687d69b9c7dac1f52
Aleksei Chapaev <alekseiachapaev@gmail.com>
2022-12-19 16:20:26 +0100

    RF testManageOldData and testPluginManagerInstallPlugin to POM create classes UpdateCenterPage and ManageOldDataPage

--- src/test/java/model/ManageJenkinsPage.java
+++ src/test/java/model/ManageJenkinsPage.java
@@ -5,6 +5,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import runner.TestUtils;
+
+import static runner.TestUtils.*;
 
 public class ManageJenkinsPage extends Header {
 
@@ -17,6 +20,12 @@ public class ManageJenkinsPage extends Header {
     @FindBy(xpath = "//div//h1")
     private WebElement header1;
 
+    @FindBy(xpath = "//a[@href  = 'administrativeMonitor/OldData/']/parent::div")
+    private WebElement linkManageOldData;
+
+    @FindBy(xpath = "//a[@href='pluginManager']")
+    private WebElement linkPluginManager;
+
     public ManageJenkinsPage(WebDriver driver) {
         super(driver);
     }
@@ -28,7 +37,8 @@ public class ManageJenkinsPage extends Header {
     }
 
     public ManageUsersPage clickManageUsers() {
-        getWait(5).until(ExpectedConditions.elementToBeClickable(manageUsers)).click();
+        scrollToElement(getDriver(), manageUsers);
+        getWait(5).until(TestUtils.ExpectedConditions.elementIsNotMoving(manageUsers)).click();
 
         return new ManageUsersPage(getDriver());
     }
@@ -37,5 +47,19 @@ public class ManageJenkinsPage extends Header {
         getWait(10).until(ExpectedConditions.visibilityOf(header1));
 
         return header1.getText();
+    }
+
+    public ManageOldDataPage clickLinkManageOldData(){
+        scrollToElement(getDriver(), linkManageOldData);
+        getWait(5).until(TestUtils.ExpectedConditions.elementIsNotMoving(linkManageOldData)).click();;
+
+        return new ManageOldDataPage(getDriver());
+    }
+
+    public PluginManagerPage clickLinkManagePlugins(){
+        scrollToElement(getDriver(), linkPluginManager);
+        getWait(5).until(TestUtils.ExpectedConditions.elementIsNotMoving(linkPluginManager)).click();;
+
+        return new PluginManagerPage(getDriver());
     }
 }
