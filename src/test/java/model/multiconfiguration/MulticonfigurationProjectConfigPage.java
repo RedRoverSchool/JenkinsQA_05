d37a6a85ad6ad9f12b5fe0a5ec97bd9f90536cc8
Janar <janarkqa@gmail.com>
2022-12-19 01:19:10 -0600

    resolve merging conflict

--- src/test/java/model/multiconfiguration/MulticonfigurationProjectConfigPage.java
+++ src/test/java/model/multiconfiguration/MulticonfigurationProjectConfigPage.java
@@ -1,16 +1,18 @@
 package model.multiconfiguration;
 
-import model.base.BaseConfigPage;
 import model.HomePage;
+import model.base.BaseConfigPage;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import runner.TestUtils;
 
-public class MulticonfigurationProjectConfigPage extends BaseConfigPage {
+public class MultiConfigurationProjectConfigPage extends BaseConfigPage {
 
-    @FindBy(css = "#breadcrumbs li a")
-    private WebElement dashboard;
+    @FindBy(xpath = "//button[@type='submit']")
+    private WebElement saveButton;
 
     @FindBy(name = "description")
     private WebElement inputDescription;
@@ -21,31 +23,74 @@ public class MulticonfigurationProjectConfigPage extends BaseConfigPage {
     @FindBy(xpath = "//div[@class='textarea-preview']")
     private WebElement previewArea;
 
-    public MulticonfigurationProjectConfigPage(WebDriver driver) {
+    @FindBy(xpath = "//div[@class='jenkins-section__title'][@id='build-steps']")
+    private WebElement buildStepsSection;
+
+    @FindBy(xpath = "//button[text()='Add build step']")
+    private WebElement addBuildStepButton;
+
+    @FindBy(id = "yui-gen29")
+    private WebElement executeWindowsFromBuildSteps;
+
+    @FindBy(id = "yui-gen38-button")
+    private WebElement advancedBuildStepsButton;
+
+    @FindBy(css = ".jenkins-input.fixed-width")
+    private WebElement textAreaBuildSteps;
+
+    public MultiConfigurationProjectConfigPage(WebDriver driver) {
         super(driver);
     }
 
-    public HomePage goToDashboard() {
-        dashboard.click();
+    public MultiConfigurationProjectStatusPage clickSave() {
+        saveButton.click();
 
-        return new HomePage(getDriver());
+        return new MultiConfigurationProjectStatusPage(getDriver());
     }
 
-    public MulticonfigurationProjectConfigPage inputDescription(String description) {
+    public MultiConfigurationProjectConfigPage inputDescription(String description) {
         inputDescription.sendKeys(description);
 
-        return new MulticonfigurationProjectConfigPage(getDriver());
+        return new MultiConfigurationProjectConfigPage(getDriver());
     }
 
-    public MulticonfigurationProjectConfigPage showPreview() {
+    public MultiConfigurationProjectConfigPage showPreview() {
         getWait(5).until(ExpectedConditions.visibilityOf(textareaShowPreview)).click();
 
-        return new MulticonfigurationProjectConfigPage(getDriver());
+        return new MultiConfigurationProjectConfigPage(getDriver());
     }
 
     public MultiConfigurationProjectStatusPage getPreview() {
         getWait(5).until(ExpectedConditions.visibilityOf(previewArea)).getText();
 
         return new MultiConfigurationProjectStatusPage(getDriver());
+    }
+
+    public MultiConfigurationProjectStatusPage clickSaveButton() {
+        getWait(5).until(ExpectedConditions.visibilityOf(saveButton)).click();
+
+        return new MultiConfigurationProjectStatusPage(getDriver());
+    }
+
+    public MultiConfigurationProjectConfigPage scrollAndClickBuildSteps() {
+        getWait(5).until(ExpectedConditions.visibilityOfElementLocated(By.tagName("h2")));
+        TestUtils.scrollToElement(getDriver(), buildStepsSection);
+        getWait(20).until(ExpectedConditions.elementToBeClickable(addBuildStepButton));
+        addBuildStepButton.click();
+
+        return this;
+    }
+
+    public MultiConfigurationProjectConfigPage selectionAndClickExecuteWindowsFromBuildSteps() {
+        executeWindowsFromBuildSteps.click();
+
+        return this;
+    }
+
+    public MultiConfigurationProjectConfigPage enterCommandInBuildSteps(String command) {
+        getWait(10).until(ExpectedConditions.elementToBeClickable(advancedBuildStepsButton));
+        textAreaBuildSteps.sendKeys(command);
+
+        return this;
     }
 }
