4e08959226ddaa2c9ef27f5beeb346fe35512ff5
Stanislaw1349 <alfstas@gmail.com>
2022-12-20 20:46:51 +0300

    add Ignore testDeleteFreestyleProjectInFolder

--- src/test/java/tests/FolderTest.java
+++ src/test/java/tests/FolderTest.java
@@ -184,6 +184,7 @@ public class FolderTest extends BaseTest {
         Assert.assertTrue(projectNamesInFolder.contains(FREESTYLE_PROJECT_NAME));
     }
 
+    @Ignore
     @Test(dependsOnMethods = "testCreateFreestyleProjectInFolderNewItem")
     public void testDeleteFreestyleProjectInFolder() {
 
