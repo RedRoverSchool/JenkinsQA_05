94dfb2d121ee2f7f229d90bee5d5728d610b7c44
RustamKhudoyarov <khrust_lord@mail.ru>
2022-12-22 22:58:55 -0600

    RF | FooterTest > Tests_move_to_POM -Deleted duplicate tests. -Modified test testFooterLinkJenkinsIsClickable() -Move all class on POM. -Footer changed extends from BasePage. Before Footer extends from Header.

--- src/test/java/model/ManageJenkinsPage.java
+++ src/test/java/model/ManageJenkinsPage.java
@@ -26,6 +26,9 @@ public class ManageJenkinsPage extends Header {
     @FindBy(xpath = "//a[@href='pluginManager']")
     private WebElement linkPluginManager;
 
+    @FindBy(linkText = "Manage Jenkins")
+    private WebElement manageJenkins;
+
     public ManageJenkinsPage(WebDriver driver) {
         super(driver);
     }
@@ -51,15 +54,23 @@ public class ManageJenkinsPage extends Header {
 
     public ManageOldDataPage clickLinkManageOldData(){
         scrollToElement(getDriver(), linkManageOldData);
-        getWait(5).until(TestUtils.ExpectedConditions.elementIsNotMoving(linkManageOldData)).click();;
+        getWait(5).until(TestUtils.ExpectedConditions.elementIsNotMoving(linkManageOldData)).click();
 
         return new ManageOldDataPage(getDriver());
     }
 
     public PluginManagerPage clickLinkManagePlugins(){
         scrollToElement(getDriver(), linkPluginManager);
-        getWait(5).until(TestUtils.ExpectedConditions.elementIsNotMoving(linkPluginManager)).click();;
+        getWait(5).until(TestUtils.ExpectedConditions.elementIsNotMoving(linkPluginManager)).click();
 
         return new PluginManagerPage(getDriver());
+    }
+
+    public ExternalJenkinsPage clickManageJenkins() {
+        manageJenkins.click();
+        scrollToEnd(getDriver());
+        getAction().pause(500).moveToElement(new ExternalJenkinsPage(getDriver()).getJenkinsLink())
+                .perform();
+        return new ExternalJenkinsPage(getDriver());
     }
 }
