4ef20fb446a1666810a90c66abcf6d4ac6b169f2
Janar <janarkqa@gmail.com>
2023-01-09 09:47:53 -0600

    fix

--- src/test/java/model/base/side_menu/BaseStatusSideMenuFrame.java
+++ src/test/java/model/base/side_menu/BaseStatusSideMenuFrame.java
@@ -3,7 +3,7 @@ package model.base.side_menu;
 import model.base.BaseStatusPage;
 import org.openqa.selenium.WebDriver;
 
-public class BaseStatusSideMenuFrame<StatusPage extends BaseStatusPage<?, ?>> extends BaseSideMenuFrame<StatusPage> {
+public abstract class BaseStatusSideMenuFrame<StatusPage extends BaseStatusPage<?, ?>> extends BaseSideMenuFrame<StatusPage> {
 
     public BaseStatusSideMenuFrame(WebDriver driver, StatusPage statusPage) {
         super(driver, statusPage);
