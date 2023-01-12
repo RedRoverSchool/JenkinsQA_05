89537a282d46c403ba971ce792ca680302f417ca
DenSebrovsky <den.sebrovsky@mail.ru>
2023-01-12 12:15:44 +0300

    ignore testBuildParametrizedProject()

--- src/test/java/tests/PipelineTest.java
+++ src/test/java/tests/PipelineTest.java
@@ -188,6 +188,7 @@ public class PipelineTest extends BaseTest {
         Assert.assertEquals(emptyErrorArea, "");
     }
 
+    @Ignore
     @Test(dependsOnMethods = "testWarningMessageIsDisappeared")
     public void testBuildParametrizedProject() {
         String consoleOutputText = new HomePage(getDriver())
